18f220de87f113d7726cc5980f1ded827969993b
treuther <tracyreuther@gmail.com>
2020-09-14 12:16:56 -0500

    added constructor and state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      items: [],
+      currentItem: {
+        text: ' ',
+        key: ' '
+      }
+    }
+  }
+  
   render() {
     return (
       <div className="App">
