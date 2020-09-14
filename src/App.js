c8aec0b7d9e6bcf3a732f766a260d45b1f9b8b1b
treuther <tracyreuther@gmail.com>
2020-09-14 04:41:39 -0500

    className addition to App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <header>
+      <div className="App">
+        <header>
         <form id="to-do-form">
           <input type="text" placeholder="Enter Text"/>
           <button type="submit">Add</button>
         </form>
       </header>
+      </div>
     );
   }
 }
