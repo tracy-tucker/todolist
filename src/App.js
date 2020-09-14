c8b036fd54a09bf8bff7d2873cc6b86a77ae4b8f
treuther <tracyreuther@gmail.com>
2020-09-14 04:35:46 -0500

    creation of basic form

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  render() {
+    return (
+      <header>
+        <form id="to-do-form">
+          <input type="text" placeholder="Enter Text"/>
+        </form>
       </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
